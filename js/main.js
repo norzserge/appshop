8bf59c27259d0477e774e8f29caf3878594092ab
Sergey Metelskiy <metelskiisa@mail.ru>
2015-02-04 08:10:37 +0300

    add toTop button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+$(document).ready(function(){
 
-
+	$(function() {
+	 
+		$(window).scroll(function() {		 
+			if($(this).scrollTop() != 0) {			 
+				$('.toTop').fadeIn();			 
+			} else {			 
+				$('.toTop').fadeOut();			 
+			}		 
+		});
+			$('.toTop').click(function() {			 
+			$('body,html').animate({scrollTop:0},800);		 
+		});
+	 
+	});
+ 
+});
 
